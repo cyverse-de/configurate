3b5fea33c1e69a1e59bb7bfe38bd9f4ec352882b
Dennis Roberts <dennis@iplantcollaborative.org>
2016-08-30 13:23:59 -0700

    CORE-8051: added a test for defaults with a missing configuration file

--- configurate_test.go
+++ configurate_test.go
@@ -6,6 +6,7 @@ import (
 
 const cfgFile = "test/test_config.yaml"
 const partialCfgFile = "test/test_partial_config.yaml"
+const missingCfgFile = "test/ekky_ekky_ekky_zboing_zoom_bang_znourringmn.yaml"
 
 func TestNew(t *testing.T) {
 	cfg, err := Init(cfgFile)
@@ -70,5 +71,26 @@ func TestDefaults(t *testing.T) {
 	expected = "irods"
 	if actual != expected {
 		t.Errorf("The defaulted irods.host was %s instead of %s", actual, expected)
+	}
+}
+
+func TestDefaultsWithNonExistentConfigFile(t *testing.T) {
+	cfg, err := InitDefaults(missingCfgFile, JobServicesDefaults)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Spot-check a configuration setting.
+	actual := cfg.GetString("apps.callbacks_uri")
+	expected := "http://apps:60000/callbacks/de-job"
+	if actual != expected {
+		t.Errorf("The defaulted apps.callbacks_uri was %s instead of %s", actual, expected)
+	}
+
+	// Spot-check another configuration setting for good measure.
+	actual = cfg.GetString("amqp.uri")
+	expected = "amqp://guest:guest@rabbit:5672/jobs"
+	if actual != expected {
+		t.Errorf("The overridden amqp.uri was %s instead of %s", actual, expected)
 	}
 }
